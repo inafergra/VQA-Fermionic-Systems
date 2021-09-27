1364719441231cf7e432bbe735fadba4fc624f05
ignacio <inafergra@gmail.com>
2021-09-27 13:44:41 +0200

    Started implementing energy of the independent syk models

--- syk_functions.py
+++ syk_functions.py
@@ -25,58 +25,90 @@ def init_syk_tensor(N, J = 1, mean=0, ):
     return J
 
 def init_TFD_model(N, J, mu):
-    J_L, J_R = init_syk_tensor(N, J = J)
-    J_int = 1j* mu * np.identity(N)
-    return J_L, J_R, J_int
+    J_L = init_syk_tensor(N, J = J)
+    J_R = J_L
+    H_int = 1j* mu * np.identity(2*N)
+    return J_L, J_R, H_int
 
 #J = init_syk_tensor(5)
 #print(J[1,2,3,4], J[3,4,1,2])
 
 
-def inter_energy(H_int):
+def energy_inter(H_int):
     '''
     Calculates the interaction energy of the TFD Hamiltonian. The energy is taken w.r.t to 
     the |phi> state. 
     '''
     N = int(np.size(H,axis=0)/2)
     energy = 0
-    for a in range(2*N):
-            for b in range(2*N):
-                for i in range(2*N):
-                        for j in range(2*N): 
-                            for l in range(2*N):
-                                for k in range(2*N):
-                                    alpha_i = i%2
-                                    alpha_j = j%2
-                                    alpha_k = k%2
-                                    alpha_l = l%2
-                                    alpha_a = a%2
-                                    alpha_b = b%2
-                                    energy -= H[2*a+alpha_a,2*b+alpha_b] * (1j**(alpha_j + alpha_k)*(-1)**(alpha_b + alpha_k) + 1j**(alpha_i + alpha_l)*(-1)**(alpha_a + alpha_l))
+    for a in range(N):
+            for b in range(N):
+                for i in range(N):
+                        for j in range(N): 
+                            for l in range(N):
+                                for k in range(N):
+                                    for alpha_i in range(2):
+                                        for alpha_j in range(2):
+                                            for alpha_k in range(2):
+                                                    for alpha_l in range(2): 
+                                                        for alpha_a in range(2):
+                                                            for alpha_b in range(2):
+                                                                energy += H[2*a+alpha_a,2*b+alpha_b] * (1j**(alpha_j + alpha_k)*(-1)**(alpha_b + alpha_k) + 1j**(alpha_i + alpha_l)*(-1)**(alpha_a + alpha_l))
     return energy
 
-def syk_energy(J):
+
+def mat_elem_syk(J, m1, m2):
     """
     Returns the energy of the independent SYK models w.r.t. the |phi> state
     """
     N = int(np.size(H,axis=0)/2)
-    energy = 0
-    for i in range(2*N):
-        for j in range(2*N): 
-            for l in range(2*N):
-                for k in range(2*N):
-                    alpha_i = i%2
-                    alpha_j = j%2
-                    alpha_k = k%2
-                    alpha_l = l%2
-                    energy += 0 #need to do the calculations
-    return energy
+    mat_elem = 0
+    for i in range(N):
+            for j in range(N):
+                for l in range(N):
+                    for k in range(N):
+                        for alpha in range(0,2):
+                            for beta in range(0,2):
+                                for gamma in range(0,2):
+                                    for delta in range(0,2):
+                                        x = J[2*l+alpha, 2*k+beta, 2*i+delta, 2*j+gamma] * 1j**(alpha+beta+gamma+delta)
+                                        if (l==k==m1==m2==i==j) or  (l==k==m1==j!=i==m2) or (k==m1!=l==i==j==m2) or (k==m1!=l==j!=i==m2):
+                                            mat_elem += x*(-1)**(alpha*delta)
+                                        elif (l==k==m1==m2!=i==j) or  (l==k==m1==i!=j==m2) or (k==m1!=l==m2!=i==j) or (k==m1!=l==i!=j==m2):
+                                            mat_elem += x*(-1)**(alpha+gamma)
+                                        elif (l==k!=m1==m2==i==j) or  (l==k!=m1==j!=i==m2) or (l==m1!=k==i==j==m2) or (l==m1!=k==j!=i==m2):
+                                            mat_elem += x*(-1)**(beta+delta)
+                                        elif (l==k!=m1==m2!=i==j) or (l==k!=m1==i!=j==m2) or (l==m1!=k==m2!=i==j) or (l==m1!=k==i!=j==m2):
+                                            mat_elem += x*(-1)**(beta+gamma)
+                                        elif (l==i!=k==j!=m1==m2) or (l==m2!=k==j!=m1==i) or (l==i!=k==m2!=m1==j):
+                                            mat_elem += -x*(-1)**(alpha+beta)
+                                        elif (l==j!=k==i!=m1==m2) or (l==m2!=k==i!=m1==j) or (l==j!=k==m2!=m1==i):
+                                            mat_elem += x*(-1)**(alpha+beta)
+    return mat_elem
 
-def appy_right_unitary(J_L, J_R, J_int, t, indices):
+
+def energy_syk(J):
+    '''
+    Computes the energy TO THE FIRST ORDER of the state given by doing an X rotation of theta[i] in qubit i. 
+    args:
+        theta: 1darray (len = number of fermions/qubits N)
+    '''
+    N = int(np.size(H,axis=0)/2)
+    e = 0
+    for m1 in range(N):
+        for m2 in range(N):
+            e += theta[m1]*theta[m2]*ham_matrix_element(H, m1, m2)
+    #print(e)
+    return energy(H) + e
+
+def tfd_energy(J_L, J_R, H_int):
+    return (syk_energy(J_R) + syk_energy(J_L) + inter_energy(H_int))
+
+def apply_unitary(J_L, J_R, H_int, t, subsystem, indices):
     '''
     Calculates the new matrix of coefficients of the interaction term after applying a left h.
     '''
-    N = int(np.size(H,axis=0)/2)
+    N = int(np.size(J_L,axis=0)/2)
     i = indices[0] ; alpha = indices[1]; j = indices[2]; beta = indices[3]
 
     #Compute the A matrix
@@ -90,9 +122,24 @@ def appy_right_unitary(J_L, J_R, J_int, t, indices):
     exp_A = I + np.sin(2*t)*A/2 - (np.cos(2*t)-1)*A2/4
     exp_A_T = I - np.sin(2*t)*A/2 - (np.cos(2*t)-1)*A2/4
 
-    #Compute new interaction matrix of coefficients
-    
-    J_int = J_int
+    print(len(A))
+    print(len(exp_A))
+    if subsystem == 'R':
+        #Compute new interaction matrix of coefficients
+        H_int = np.matmul(H_int,exp_A)
+
+        #Compute new matrix for the R system
+        #J_R = np.tensordot(np.tensordot(np.tensordot(np.tensordot(J_R, exp_A, axes=([0,1])), exp_A, axes=([0,1])), exp_A, axes=([0,1])), exp_A, axes=([0,1]))
+        J_R = np.einsum('ijkl, ...i, ...j, ...k, ...l', J_R, exp_A, exp_A, exp_A, exp_A)
 
 
-    return J_L, J_R, J_int
+    elif subsystem == 'L':
+        #Compute new interaction matrix of coefficients
+        H_int = exp_A_T @ H_int 
+
+        #Compute new matrix for the L system
+        #J_L = np.tensordot(np.tensordot(np.tensordot(np.tensordot(J_L, exp_A, axes=([0,1])), exp_A, axes=([0,1])), exp_A, axes=([0,1])), exp_A, axes=([0,1]))
+        J_L = np.einsum('ijkl, ...i, ...j, ...k, ...l', J_L, exp_A, exp_A, exp_A, exp_A)
+
+    return J_L, J_R, H_int
+
--- tfd_preparation.py
+++ tfd_preparation.py
@@ -10,20 +10,15 @@ np.random.seed(seed=3)
 N = 5
 
 #------------------Initial state
-J_L = init_syk_tensor(N)
-J_R = init_syk_tensor(N)
-#print('Initial H:')
-#print(H)
-init_energy = energy(J_L)
+J_L, J_R, H_int = init_TFD_model(2, 1, 0.1)
+
+init_energy = tfd_energy(J_L, J_R, H_int)
 print(f'Initial energy is {init_energy}')
 
-new_H_eq_mix = H
-new_H_num = H
+J_L, J_R, H_int = apply_unitary(J_L, J_R, H_int, 1, 'L', (1,0,1,1))
 
-energy_list_mix = []
-energy_list_num = []
-energy_list_num.append(init_energy)
-#energy_list_eq.append(init_energy)
+
+energy_list = []
 num_gates = 3000
 
 for k in range(num_gates):
@@ -36,53 +31,24 @@ for k in range(num_gates):
     
     #----------------------------------------Optimizing the time---------------------
     t0=np.random.rand() #initial guess
-    
     minimize_dictionary = minimize(new_energy, x0=t0,args=(new_H_num,indices), options={'disp': False}, method = 'Nelder-Mead')
     #minimize_dictionary = differential_evolution(new_energy,args=(new_H,indices), bounds=[(0,2)], disp = False)#, maxiter=10000)
 
     optimal_time_num = minimize_dictionary['x'][0]
     #print(f'Optimal time num: {optimal_time_num}')
 
-    #-------------------------------------Computing exact optimal t-------------------
-    
-    #minimize_dictionary = minimize(f, x0=t0,args=(indices,new_H), options={'disp': False}, method = 'Nelder-Mead')
-    #optimal_time = minimize_dictionary['x'][0]
-    #optimal_time_eq = optimal_t(indices,new_H_eq)
-    #optimal_time_eq_shifted = optimal_time_eq - np.pi
-    #print(f'Optimal time eq: {optimal_time_eq}')
     #--------------------------------------Computing energy after h---------------------
     new_H_num = appy_h_gate(optimal_time_num,new_H_num, indices)
     final_energy_num = energy(new_H_num)
-
-    #new_H_eq = appy_h_gate(optimal_time_eq,new_H_eq, indices)
-    #final_energy_eq= energy(new_H_eq)
-
-    #new_H_eq_shifted = appy_h_gate(optimal_time_eq_shifted,new_H_eq, indices)
-    #final_energy_eq_shifted = energy(new_H_eq_shifted)
-
-    #if final_energy_eq_shifted < final_energy_eq:
-    #    final_energy_eq = final_energy_eq_shifted
-    #    new_H_eq = new_H_eq_shifted
-    #    print('Heyyyyy')
-
-    #print(f'Exact energy :{final_energy_eq}')
-    #print(f'Numeric energy :{final_energy_num}')
-
-    #energy_list_eq.append(final_energy_eq)
     energy_list_num.append(final_energy_num)
     #print(energy_list)
 
 #num_gates = len(energy_list)
-print(new_H_num)
-exact_energies = exact_energy_levels(H,2)
-theta = np.zeros(N)
-print(f'Exact ground energy: {exact_energies[0]}')
+#print(new_H_num)
+#exact_energies = exact_energy_levels(H,2)
+#theta = np.zeros(N)
+#print(f'Exact ground energy: {exact_energies[0]}')
 print(f'Numerical energy is {final_energy_num}')
-#print(f'Energy function rotate {energy_after_x_rotations(theta, new_H_num)}')
-#print(f'Variance is {squared_hamiltonian_average(new_H_num) - energy(new_H_num)**2}')#{variance(np.zeros(N), new_H_num)}')
-#print(f'Variance rotating is {variance(theta, new_H_num)}')
-#plt.plot(range(round(num_gates/2), num_gates), energy_list_num[-round(num_gates/2):], label = 'Algorithmic cooling numeric')
-#plt.plot(range(round(num_gates/2), num_gates), energy_list_exact[-round(num_gates/2):], label = 'Algorithmic cooling exact')
 
 plt.plot(energy_list_num, label = 'Algorithmic cooling numeric')
 #plt.plot(energy_list_eq, label = 'Algorithmic cooling exact')
